f8419e7dc1bfa768b0c251595b6a0a3904863c6e
112534094-scsvmv <112534094@kanchiuniv.ac.in>
2026-02-06 10:28:33 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int n,i,search,found=0;
+	printf("enter number of elements in the array:");
+	scandf("%d",&n);
+    int arr [n];
+	Printf("enter elements of the array:\n");
+	for(i=0;i<n;i++);
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the elements to search");
+    scanf("%d",&seacrh);
+    for(i=0;i<n;i++){
+    	if(arr[i]==search){
+    		found=1;
+    		printf("elements %d found at index %d\n",search,i);
+    		break;
+        }   
+    }
+    if(!found){
+    	printf("element %d not found in the array\n",search);
+    }
+    return 0;
+}
+    
+    	
