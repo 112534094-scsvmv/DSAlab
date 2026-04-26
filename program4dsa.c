a9999a0737a0e5e46e57482dec12bbade1b41137
112534094-scsvmv <112534094@kanchiuniv.ac.in>
2026-04-26 19:47:58 +0530

    Update program4dsa.c

--- program4dsa.c
+++ program4dsa.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
-
 int main()
 {
-    int n,i,j, temp, minIndex;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
+    int n, temp, minindex;
+    printf(“enter number of elements: “);
+    scanf(“%d”, &n);
     int arr[n];
-
-    printf("Enter %d elements:\n", n);
+    printf(“enter %d elements:\n”, n);
     for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    // Selection Sort logic
-    for (int i = 0; i < n - 1; i++)
+    scanf(“%d”, &arr[i]);
+    // selection sort logic
+    for (int i = 0; i < n – 1; i++)
     {
-        minIndex = i;
-
+        minindex = i;
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
+            if (arr[j] < arr[minindex])
+            minindex = j;
         }
-
-        // Swap
+        // swap
         temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        arr[i] = arr[minindex];
+        arr[minindex] = temp;
     }
-
-    printf("Sorted array:\n");
+    printf(“sorted array:\n”);
     for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-
+    printf(“%d “, arr[i]);
     return 0;
 }
-
