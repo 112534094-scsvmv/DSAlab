8a4fea2f2cebf852fa75271c955f51769af47ec9
112534094-scsvmv <112534094@kanchiuniv.ac.in>
2026-04-26 19:34:23 +0530

    Update program2dsa.c

--- program2dsa.c
+++ program2dsa.c
@@ -1,46 +1,40 @@
-#include<stdio.h>
-
-int binarySearch(int arr[], int n, int key)
+ #include<stdio.h>
+int binarysearch(int arr[],int n, int key)
 {
-    int low = 0, high = n - 1, mid;
-
-    while (low <= high)
-    {
-        mid = (low + high) / 2;
-
-        if (arr[mid] == key)
-            return mid;           // Element found
-        else if (arr[mid] < key)
-            low = mid + 1;        // Search right half
-        else
-            high = mid - 1;       // Search left half
-    }
-    return -1;  // Element not found
+int low=0,high=n-1,mid; 
+while(low<=high) 
+{ 
+mid=(low+high)/2; 
+if (arr[mid]==key)
+return mid;
+//element found
+else if(arr[mid]<key)
+low=mid+1;
+//search right half
+else
+high=mid-1;
+//search left half
 }
-
-int main()
+return 1;
+//element not found
+} 
+int main() 
 {
-    int n, key, result;
-
-    printf("Enter number of elements:");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter %d sorted elements:\n", n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    printf("Enter element to search: ");
-    scanf("%d", &key);
-
-    result = binarySearch(arr, n, key);
-
-    if (result != -1)
-        printf("Element found at index %d\n", result);
-    else
-        printf("Element not found\n");
-
-    return 0;
+int n,key,result; 
+printf(“enter number of element:”); 
+scanf(“%d”,&n); 
+int arr[n]; 
+printf(“enter %d sorted element:\n”,n);
+int i;
+for(i=0;i<n;i++){ 
+scanf(“%d”,&arr[i]);
+} 
+printf(“enter element to search:”); 
+scanf(“%d”,&key); 
+result = binarysearch(arr, n, key); 
+if(result!=-1) 
+printf(“element found at index %d \n”,result);
+else
+printf(“element not found \n”);
+return 0;
 }
-
